aa9c9a74eddbfdaf8d2194daeea495bbbe30f666
Bhavya Sachdeva <arnavtanwar15@gmail.com>
2026-04-22 18:05:11 +0530

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -14,7 +14,7 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
   useEffect(() => {
     if (birthdayReached) return;
 
-    const targetDate = new Date("2027-04-21T00:00:00");
+    const targetDate = new Date("2026-04-22T18:06:15");
 
     const updateCountdown = () => {
       const now = new Date();
